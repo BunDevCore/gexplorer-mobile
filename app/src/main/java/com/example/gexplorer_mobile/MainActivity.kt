13bc616232e079ede0348fe8bd75283849f78822
wiKapo <71190167+wiKapo@users.noreply.github.com>
2024-02-29 09:03:10 +0100

    NavigationBar changes to NavigationRail on orientation change

--- app/src/main/java/com/example/gexplorer_mobile/MainActivity.kt
+++ app/src/main/java/com/example/gexplorer_mobile/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.gexplorer_mobile
 
+import android.content.res.Configuration.*
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -23,6 +29,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -34,11 +42,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+//import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -64,13 +75,99 @@ class MainActivity : ComponentActivity() {
                     var selectedTab by rememberSaveable {
                         mutableStateOf(Screen.Main.route)
                     }
-                    Scaffold(
-                        bottomBar = {
-                            NavigationBar {
-                                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                                val currentDestination = navBackStackEntry?.destination
+                    val configuration = LocalConfiguration.current
+
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentDestination = navBackStackEntry?.destination
+                    if (configuration.orientation == ORIENTATION_PORTRAIT) {
+                        Scaffold(
+                            topBar = {
+
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(56.dp)
+                                        .background(color = colorResource(id = R.color.primary)),
+                                    horizontalArrangement = Arrangement.Center,
+                                    verticalAlignment = Alignment.CenterVertically,
+
+                                    ) {
+                                    Text(
+                                        text = stringResource(id = R.string.app_name),
+                                        fontSize = 18.sp,
+                                        color = colorResource(id = R.color.black)
+                                    )
+                                    Image(
+                                        painter = painterResource(id = R.drawable.gexplorer_logo),
+                                        contentDescription = null,
+                                        modifier = Modifier.padding(all = 2.dp)
+                                    )
+                                }
+                            },
+                            bottomBar = {
+                                NavigationBar {
+                                    items.forEach { screen ->
+                                        NavigationBarItem(
+                                            icon = {
+                                                Icon(
+                                                    imageVector = if (selectedTab == screen.route) {
+                                                        screen.iconFilled
+                                                    } else {
+                                                        screen.iconOutline
+                                                    },
+                                                    contentDescription = null
+                                                )
+                                            },
+                                            label = { Text(stringResource(screen.resourceId)) },
+                                            selected = currentDestination?.hierarchy?.any
+                                            { navDest -> navDest.route == screen.route } == true,
+                                            onClick = {
+                                                selectedTab = screen.route
+                                                navController.navigate(screen.route) {
+                                                    // Pop up to the start destination of the graph to
+                                                    // avoid building up a large stack of destinations
+                                                    // on the back stack as users select items
+                                                    popUpTo(navController.graph.findStartDestination().id) {
+                                                        saveState = true
+                                                    }
+                                                    // Avoid multiple copies of the same destination when
+                                                    // reselecting the same item
+                                                    launchSingleTop = false
+                                                    // Restore state when reselecting a previously selected item
+                                                    restoreState = true
+                                                }
+                                            }
+                                        )
+
+                                    }
+                                }
+
+                            }
+                        ) { innerPadding ->
+                            NavHost(
+                                navController,
+                                startDestination = Screen.Main.route,
+                                modifier = Modifier.padding(innerPadding)
+                            ) {
+                                composable(Screen.Main.route) { MainPage() }
+                                composable(Screen.Map.route) { MapPage() }
+                                composable(Screen.Account.route) { AccountPage() }
+                                composable(Screen.Settings.route) { SettingsPage() }
+                            }
+
+                        }
+                    } else {
+                        Row (modifier = Modifier.fillMaxSize()){
+                            NavigationRail {
+                                Image(
+                                    painter = painterResource(id = R.drawable.gexplorer_logo),
+                                    contentDescription = null,
+                                    modifier = Modifier
+                                        .padding(top = 6.dp, bottom = 6.dp)
+                                        .height(56.dp)
+                                )
                                 items.forEach { screen ->
-                                    NavigationBarItem(
+                                    NavigationRailItem(
                                         icon = {
                                             Icon(
                                                 imageVector = if (selectedTab == screen.route) {
@@ -87,36 +184,26 @@ class MainActivity : ComponentActivity() {
                                         onClick = {
                                             selectedTab = screen.route
                                             navController.navigate(screen.route) {
-                                                // Pop up to the start destination of the graph to
-                                                // avoid building up a large stack of destinations
-                                                // on the back stack as users select items
                                                 popUpTo(navController.graph.findStartDestination().id) {
                                                     saveState = true
                                                 }
-                                                // Avoid multiple copies of the same destination when
-                                                // reselecting the same item
                                                 launchSingleTop = true
-                                                // Restore state when reselecting a previously selected item
                                                 restoreState = true
                                             }
                                         }
                                     )
-
                                 }
                             }
+                            NavHost(
+                                navController,
+                                startDestination = Screen.Main.route
+                            ) {
+                                composable(Screen.Main.route) { MainPage() }
+                                composable(Screen.Map.route) { MapPage() }
+                                composable(Screen.Account.route) { AccountPage() }
+                                composable(Screen.Settings.route) { SettingsPage() }
+                            }
                         }
-                    ) { innerPadding ->
-                        NavHost(
-                            navController,
-                            startDestination = Screen.Main.route,
-                            modifier = Modifier.padding(innerPadding)
-                        ) {
-                            composable(Screen.Main.route) { MainPage() }
-                            composable(Screen.Map.route) { MapPage() }
-                            composable(Screen.Account.route) { AccountPage() }
-                            composable(Screen.Settings.route) { SettingsPage() }
-                        }
-
                     }
                 }
             }
@@ -149,6 +236,16 @@ val items = listOf(
     Screen.Account,
     Screen.Settings
 )
+
+//@Composable
+//fun NavigationBarLayout(
+//    destination: NavDestination,
+//    onMenuItemSelected: (String) -> Unit,
+//    content: @Composable (innerPadding: PaddingValues) -> Unit
+//) {
+//
+//
+//}
 
 @Composable
 fun MainPage() {
