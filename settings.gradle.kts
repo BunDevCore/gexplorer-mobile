46b0d9ce38a99851608e5cf5355f46df1b2aa0a6
wiKapo <71190167+wikapo@users.noreply.github.com>
2024-03-05 16:47:55 +0100

    Mapbox beginnings
    [gradle problems included ;)]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
+import java.io.FileInputStream
+import java.net.URI
+import java.util.Properties
+
+fun magic(key: String): String {
+
+    val propFile = File(settings.rootDir.path, "local.properties")
+    val properties = Properties()
+    properties.load(FileInputStream(propFile))
+    return properties[key] as String
+
+
+}
+
 pluginManagement {
     repositories {
         google()
@@ -10,9 +24,18 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = URI.create("https://api.mapbox.com/downloads/v2/releases/maven")
+            authentication {
+                BasicAuthentication { "kutas" }
+            }
+            credentials {
+                username = "infinifen"
+                password = magic("MAP_API_KEY")
+            }
+        }
     }
 }
 
 rootProject.name = "gexplorer-mobile"
 include(":app")
- 
