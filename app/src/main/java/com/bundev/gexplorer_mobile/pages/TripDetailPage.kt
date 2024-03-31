bb63142897337628a0fe25057de8721a50360877
wiKapo <71190167+wiKapo@users.noreply.github.com>
2024-03-31 03:00:33 +0200

    Trip detail try no.1

--- app/src/main/java/com/bundev/gexplorer_mobile/pages/TripDetailPage.kt
+++ app/src/main/java/com/bundev/gexplorer_mobile/pages/TripDetailPage.kt
@@ -1,23 +1,35 @@
 package com.bundev.gexplorer_mobile.pages
 
+import android.graphics.Color
 import androidx.compose.runtime.Composable
 import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
 import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
+import com.mapbox.maps.extension.style.layers.generated.lineLayer
 import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
 
 @OptIn(MapboxExperimental::class)
 @Composable
 fun TripDetailPage(tripId: String?) {
     //TODO Fetch points from backend using tripId
+    val mapViewportState = MapViewportState
+    val mapView = MapView
     MapboxMap(
         mapViewportState = MapViewportState().apply {
             setCameraOptions {
                 center(Point.fromLngLat(18.6650564007217, 54.29906183330589))
+                pitch(0.0)
+                bearing(0.0)
+                zoom(16.0)
             }
         }
     ) {
-        geoJsonSource(id = "Trip") {data("zabawny.URL.thefen.mailto.me", "HIHIHIHA")}
+        geoJsonSource(id = "Trip") { data("asset://gdansk_4326.geojson") }
+        lineLayer("line-layer", "Trip") {
+            lineColor(Color.RED)
+            lineWidth(1.0)
+        }
     }
 }
