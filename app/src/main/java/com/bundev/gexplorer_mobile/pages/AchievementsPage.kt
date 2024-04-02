927af8745d465aea189c373c1bfb7ae8e0c9e6ab
wiKapo <71190167+wiKapo@users.noreply.github.com>
2024-04-03 00:00:48 +0200

    AchievementPage prototype done

--- app/src/main/java/com/bundev/gexplorer_mobile/pages/AchievementsPage.kt
+++ app/src/main/java/com/bundev/gexplorer_mobile/pages/AchievementsPage.kt
@@ -1,9 +1,13 @@
 package com.bundev.gexplorer_mobile.pages
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -21,10 +25,10 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,86 +36,62 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.bundev.gexplorer_mobile.GexplorerIcons
-import com.bundev.gexplorer_mobile.R
 import com.bundev.gexplorer_mobile.classes.Achievement
 import com.bundev.gexplorer_mobile.icons.filled.Map
 import com.bundev.gexplorer_mobile.icons.filled.Trophy
 import com.bundev.gexplorer_mobile.icons.simple.Walk
+import com.bundev.gexplorer_mobile.ui.GroupingList
 import kotlinx.datetime.Clock
 import kotlinx.datetime.Instant
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import java.text.DateFormat
 
+val achievements = listOf(
+    Achievement(
+        name = "Moje pierwsze osiągnięcie",
+        description = "I jego opis",
+        timeAchieved = Clock.System.now()
+    ),
+    Achievement(
+        name = "short",
+        timeAchieved = Clock.System.now(),
+        imageVector = GexplorerIcons.Simple.Walk
+    ),
+    Achievement(
+        timeAchieved = Clock.System.now(),
+        name = "Bardzo długa nazwa nie wiem po co, ale się domyślam",
+        description = "I jego opis, który też zajmie co najmniej 2 linijki, a może nawet i 3 to będzie zajmować",
+        imageVector = GexplorerIcons.Filled.Map
+    ),
+    Achievement(name = "Nowe osiągnięcie, którego nikt jeszcze nie ma!!!"),
+    Achievement(name = "Ta? to pa"),
+    Achievement(name = "nice ok"),
+    Achievement(name = "bruh"),
+    Achievement(name = "wtf is going on here", imageVector = Icons.Default.Close),
+    Achievement(
+        name = "Hello world",
+        description = "Open the app for the first time",
+        imageVector = Icons.Default.Favorite
+    ),
+    Achievement(
+        name = "KOLEJNE\nDŁUGIE\nBEZSENSOWNE\nOSIĄGNIĘCIE",
+        description = "Bo chcę sprawdzić, czy scroll działa"
+    ),
+    Achievement(name = "69 nice")
+)
+
 @Composable
 fun AchievementsPage() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        val achievements = listOf(
-            Achievement(
-                name = "Moje pierwsze osiągnięcie",
-                description = "I jego opis",
-                timeAchieved = Clock.System.now()
-            ),
-            Achievement(
-                name = "short",
-                timeAchieved = Clock.System.now(),
-                imageVector = GexplorerIcons.Simple.Walk
-            ),
-            Achievement(
-                timeAchieved = Clock.System.now(),
-                name = "Bardzo długa nazwa nie wiem po co, ale się domyślam",
-                description = "I jego opis, który też zajmie co najmniej 2 linijki, a może nawet i 3 to będzie zajmować",
-                imageVector = GexplorerIcons.Filled.Map
-            ),
-            Achievement(name = "Nowe osiągnięcie, którego nikt jeszcze nie ma!!!"),
-            Achievement(name = "Ta? to pa"),
-            Achievement(name = "nice ok"),
-            Achievement(name = "bruh"),
-            Achievement(name = "wtf is going on here", imageVector = Icons.Default.Close),
-            Achievement(
-                name = "Hello world",
-                description = "Open the app for the first time",
-                imageVector = Icons.Default.Favorite
-            ),
-            Achievement(
-                name = "KOLEJNE\nDŁUGIE\nBEZSENSOWNE\nOSIĄGNIĘCIE",
-                description = "Bo chcę sprawdzić, czy scroll działa"
-            ),
-            Achievement(name = "69 nice")
-        )
-        AchievementsList(achievements)
-    }
-}
-
-@Composable
-fun AchievementsList(achievements: List<Achievement>) {
-    val achievementSection =
-        achievements.groupBy { it.timeAchieved.toLocalDateTime(TimeZone.currentSystemDefault()).date }
     Column(
-        modifier = Modifier.verticalScroll(rememberScrollState())
-    ) {
-        achievementSection.forEach {
-            AchievementSection(it.value)
-        }
-    }
-}
-
-@Composable
-fun AchievementSection(achievements: List<Achievement>) {
-    Card(
         modifier = Modifier
-            .padding(horizontal = 10.dp)
-            .padding(top = 10.dp)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
-        val label = if (achievements[0].timeAchieved > Instant.DISTANT_PAST)
-            formatDate(achievements[0].timeAchieved)
-        else
-            stringResource(id = R.string.locked_achievements)
-        Text(
-            modifier = Modifier.padding(horizontal = 15.dp, vertical = 7.5.dp),
-            text = label
-        )
-        achievements.forEach { achievement ->
+        GroupingList(
+            items = achievements,
+            groupBy = { it.timeAchieved.toLocalDateTime(TimeZone.currentSystemDefault()).date },
+            title = { formatDate(it) }) { achievement ->
             val openAchievementDialog = remember { mutableStateOf(false) }
             when {
                 openAchievementDialog.value -> {
@@ -120,7 +100,9 @@ fun AchievementSection(achievements: List<Achievement>) {
                     }
                 }
             }
-            AchievementItem(achievement = achievement) { openAchievementDialog.value = true }
+            AchievementItem(achievement = achievement) {
+                if (achievement.description is String) openAchievementDialog.value = true
+            }
         }
     }
 }
@@ -143,7 +125,8 @@ fun AchievementItem(achievement: Achievement, onClick: () -> Unit) {
             .padding(bottom = 7.5.dp),
         onClick = { onClick() },
         shape = RoundedCornerShape(20.dp),
-        contentPadding = PaddingValues(0.dp)
+        contentPadding = PaddingValues(0.dp),
+        enabled = achievement.description is String
     ) {
         ListItem(
             headlineContent = {
@@ -177,27 +160,26 @@ fun AchievementDialog(achievement: Achievement, onDismissRequest: () -> Unit) {
         onDismissRequest = onDismissRequest,
         properties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
-//        AchievementDetailPage(achievement)
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(10.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = achievement.name,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(text = achievement.description!!)
+            }
+        }
     }
-}
-
-@Composable
-fun AchievementDetailPage(achievement: Achievement) {
-    Card{}
-}
-
-@Preview
-@Composable
-fun AchievementCardPreview() {
-    val achievements = listOf(
-        Achievement(
-            name = "Przykładowe długie osiągnięcie jest tu wpisane",
-            description = "SIEMANECZKO",
-            timeAchieved = Clock.System.now()
-        ),
-        Achievement(name = "hi", timeAchieved = Clock.System.now())
-    )
-    AchievementsList(achievements)
 }
 
 @Preview(locale = "pl")
