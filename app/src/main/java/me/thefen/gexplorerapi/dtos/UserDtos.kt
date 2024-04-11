1f58730cd05f1ecd8fd60da6953b632d67a5d52c
wiKapo <71190167+wiKapo@users.noreply.github.com>
2024-04-11 18:31:43 +0200

    Update UserDto

--- app/src/main/java/me/thefen/gexplorerapi/dtos/UserDtos.kt
+++ app/src/main/java/me/thefen/gexplorerapi/dtos/UserDtos.kt
@@ -1,8 +1,9 @@
 package me.thefen.gexplorerapi.dtos
 
-import java.util.*
+import java.util.Date
+import java.util.UUID
 
 data class UserDto(val id: UUID, val username: String, val overallAreaAmount: Double, val joinedAt: Date,
-    val trips: List<TripDto>, val tripAmount: Int, val districtAreas: Map<UUID, Double>, val achievements: List<AchievementGetDto>)
+    val trips: List<TripDto>, val tripAmount: Int, val totalTripLength: Double, val districtAreas: Map<UUID, Double>, val achievements: List<AchievementGetDto>)
 
 data class ShortUserDto(val id: UUID, val username: String, val overallAreaAmount: Double, val joinedAt: Date)
